c0ef957984d944b1fc35b917a66af2678a285380
Bertrik Sikken <bertrik@sikken.nl>
2025-02-16 21:33:26 +0100

    Refactor, add indication of queued duration of most recent job.

--- gitlabstatuslight/gitlabstatuslight.py
+++ gitlabstatuslight/gitlabstatuslight.py
@@ -14,18 +14,27 @@ import requests
 class GitlabCLient():
     """ Retrieves pipeline build status from gitlab """
 
-    def __init__(self, host):
+    def __init__(self, host : str, projectid: str, token: str):
         self.host = host
+        self.projectid = projectid
+        self.token = token
 
-    def get_latest_pipeline(self, projectid, token, ref):
-        url = f"https://{self.host}/api/v4/projects/{projectid}/pipelines/latest"
-        headers = {"PRIVATE-TOKEN": token}
+    def get_latest_pipeline(self, ref: str):
+        url = f"https://{self.host}/api/v4/projects/{self.projectid}/pipelines/latest"
+        headers = {"PRIVATE-TOKEN": self.token}
         params = {"ref": ref}
         response = requests.get(url, headers=headers, params=params, timeout=20)
         if not response.ok:
             print(f"GET of {url} for ref '{ref}' failed: {response.status_code} - {response.reason}")
         return response.json()
 
+    def get_jobs(self):
+        url = f"https://{self.host}/api/v4/projects/{self.projectid}/jobs"
+        headers = {"PRIVATE-TOKEN" : self.token}
+        response = requests.get(url, headers=headers, timeout=20)
+        if not response.ok:
+            print(f"GET of {url} failed: {response.status_code} - {response.reason}")
+        return response.json()
 
 class PatliteClient():
     """ Communicates with a Patlite LA6 """
@@ -43,7 +52,7 @@ class PatliteClient():
         PINK = 8
         WHITE = 9
 
-    def __init__(self, host):
+    def __init__(self, host: str):
         self.host = host
 
     def send_control(self, params):
@@ -58,6 +67,16 @@ class PatliteClient():
         params = {"color": code, "buzzer": 0}
         self.send_control(params)
 
+def queue_duration_to_color(jobs) -> PatliteClient.Color:
+    if len(jobs) == 0:
+        return PatliteClient.Color.BLUE
+    queued_duration = float(jobs[0]["queued_duration"])
+    print(f"Most recent queued duration = {queued_duration}")
+    if queued_duration < 20:
+        return PatliteClient.Color.GREEN
+    if queued_duration < 120:
+        return PatliteClient.Color.AMBER
+    return PatliteClient.Color.RED
 
 def main():
     """ The main entry point """
@@ -68,18 +87,20 @@ def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-gitlab", help="The gitlab instance host name",
                         default="gitlab.technolution.nl")
-    parser.add_argument("-t", "--token", help="The gitlab API private token", default="")
     parser.add_argument("-i", "--projectid", help="The gitlab project id", default="197")
+    parser.add_argument("-t", "--token", help="The gitlab API private token", default="")
     parser.add_argument("-patlite", help="The patlist host name", default="localhost:8000")
     args = parser.parse_args()
 
-    gitlab = GitlabCLient(args.gitlab)
+    gitlab = GitlabCLient(args.gitlab, args.projectid, args.token)
     patlite = PatliteClient(args.patlite)
     refs = ["master", "releases/1.58.x", "releases/1.56.x"]
     while True:
         colors = []
+
+        # build status
         for ref in refs:
-            result = gitlab.get_latest_pipeline(args.projectid, args.token, ref)
+            result = gitlab.get_latest_pipeline(ref)
             status = result.get("status", "undefined")
             match status:
                 case "success":
@@ -90,6 +111,11 @@ def main():
                     color = PatliteClient.Color.BLUE
             print(f"{ref}: {status} -> {color}")
             colors.append(color)
+
+        # job queue status
+        jobs = gitlab.get_jobs()
+        colors.append(queue_duration_to_color(jobs))
+
         patlite.set_color(colors)
 
         time.sleep(300)
